df3a437f5400b5266b332dab4c275a6a8f7b35a6
marinaAC <maru.cardozo02@gmail.com>
2017-03-23 20:54:37 -0300

    clase laboratorio

---
+++ Laboratorio-Programacion/clase2/ejercicio2/ejercicio2/miBiblioteca.c
@@ -0,0 +1,9 @@
+
+int obtenerInt(char mensaje[],char mensajeError[],int maximo, int minimo){
+    //pasar maximos y minimos para saber el limite del valor de lo que se pide
+    int buffer;
+    printf(mensaje);
+    scanf("%d",&buffer);
+
+    return;
+}
